a89da475b745f0cb803fce46a54adf726ebdce5c
Brendon Zach <brendon_zach@outlook.com>
2022-02-09 14:13:03 -0700

    Added most of the code and did lots

--- TextField/ViewController.swift
+++ TextField/ViewController.swift
@@ -6,14 +6,65 @@
 //
 
 import UIKit
+//Corner radius Extension for the Button.
+@IBDesignable extension UIButton {
 
-class ViewController: UIViewController {
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
 
-    override func viewDidLoad() {
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
+class ViewController: UIViewController
+{
+   //declare outlet variables for the label and text field
+    //Public or Global Variables. This means I can use these throughout the entire class.
+    
+    @IBOutlet weak var labelOutlet: UILabel!
+    
+    @IBOutlet weak var textOutlet: UITextField!
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    //This function takes the text form the text from the text field and displays it on the label when the button is pressed.
+    @IBAction func buttonToLabelFunction(_ sender: UIButton)
+    {
+        //The double question mark is called an optional.  It stores the data to the right of the double questionmark as a deafult value if nothing is entered.
+        labelOutlet.text = textOutlet.text ?? "Brendon"
+        
+        //Local Variable. Can only be used in this function.
+        
+        var BrendonisCool = textOutlet.text
+        
+    }
+    
 }
 
